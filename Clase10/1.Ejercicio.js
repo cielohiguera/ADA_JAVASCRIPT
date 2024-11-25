23669daf935473734b9a3acf2177049755394262
cieloaholiva <CH15040398@gmail.com>
2024-11-25 16:36:15 -0700

    Agregando ejercicio 1

---
+++ Clase10/1.Ejercicio.js
@@ -0,0 +1,5 @@
+// Ejercicio 1: Declarar dos variables
+// Declarar dos variables: una con el nombre edad y la otra con el nombre del peso
+let weight = 55; //Kilogramos
+let age = 25; // años
+console.log(weight, age);
